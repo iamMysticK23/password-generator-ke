b2c0a26eaea807d4be0a39171fac4c58d64c55c7
Kenai Epps <k.epps23@gmail.com>
2023-10-23 18:10:10 -0700

    light/dark mode, char selectors/number selector

--- README.md
+++ README.md
@@ -7,5 +7,10 @@ Technologies Used:
 - CSS3 (Styling)
 - JavaScript (Password generation logic)
 
+UPDATED 10/23/2023
+- Added Light/Dark Mode toggle
+- Added drop down for character selection: 10 to 20 characters
+- Added toggle switches for the use of special characters and/or numbers
+
 Author: Kenai Epps
 This was a part of Scrimba's Front End Developer Career Path as a solo project
--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-// characters to create random password generator
+//global variables
+const letters = [
+  "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+  "U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n",
+  "o","p","q","r","s","t","u","v","w","x","y","z"
+];
 
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
-"U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
-"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const numbers = [
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+];
+
+const specialCharacters = [
+  "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+  "/"
+];
+
+
 
 // declare password button and the password elements
 const passwordButton = document.getElementById("password-button")
 const password1Element = document.getElementById("password-1")
 const password2Element = document.getElementById("password-2")
+const passwordLengthSelect = document.getElementById("password-length")
+const specialCharToggle = document.getElementById("special-char-toggle")
+const numbersToggle = document.getElementById("numbers-toggle")
 
 // random password function
-function generatePassword(length) {
-    let password = ''
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length)
-        password += characters[randomIndex]
-    }
-    return password
+function generatePassword(length, useSpecialCharacters, useNumbers) {
+  let password = '';
+  let charSet = [...letters];  
+
+  if (useSpecialCharacters) {
+      charSet = [...charSet, ...specialCharacters];
+  }
+  
+  if (useNumbers) {
+      charSet = [...charSet, ...numbers];
+  }
+
+  for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charSet.length);
+      password += charSet[randomIndex];
+  }
+  return password;
 }
+
 
 // update password 1 and password 2 elements with the generated passwords
 function updatePasswords() {
-    const password1 = generatePassword(15) // generates a 15 character password
-    const password2 = generatePassword(15) // generates a 15 character password
+    const passwordLength = parseInt(passwordLengthSelect.value)
+    const useSpecialCharacters = specialCharToggle.checked
+    const useNumbers = numbersToggle.checked
+
+    const password1 = generatePassword(passwordLength, useSpecialCharacters, useNumbers)
+    const password2 = generatePassword(passwordLength, useSpecialCharacters, useNumbers)
 
     password1Element.textContent = password1
     password2Element.textContent = password2
@@ -55,4 +84,22 @@ password2Element.addEventListener('click', function() {
     const password2Text = password2Element.textContent
     clipboardCopy(password2Text)
 })
+
+// Dark mode toggle
+document.querySelector('#darkmode-toggle').addEventListener('change', function() {
+  const darkModeText = document.querySelector('.dark-mode-text');
+  const lightModeText = document.querySelector('.light-mode-text');
+
+  if (this.checked) {
+    darkModeText.classList.add('active');
+    lightModeText.classList.remove('active');
+  } else {
+    lightModeText.classList.add('active');
+    darkModeText.classList.remove('active');
+  }
+
+  document.body.classList.toggle('dark-mode');
+});
+
+
   
